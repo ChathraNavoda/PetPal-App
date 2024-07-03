cb3b4e6b7558b3954143e8933a98b66f7fe0a2fb
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2024-07-03 22:13:19 +0530

    Firebase configuration

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:petpal/app_view.dart';
 import 'package:petpal/blocs/authentication_bloc/authentication_bloc.dart';
 import 'package:user_repository/user_repository.dart';
 
@@ -10,7 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return RepositoryProvider<AuthenticationBloc>(
-        create: (context) =>
-            AuthenticationBloc(userRepository: userRepository));
+      create: (context) => AuthenticationBloc(userRepository: userRepository),
+      child: const MyAppView(),
+    );
   }
 }
