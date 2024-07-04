f3b39192a2d3d2a877d64ffd4ea965fdb5822591
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2024-07-04 16:37:22 +0530

    Create Home screen and design App bar

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:petpal/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:petpal/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 import 'package:petpal/screens/auth/views/welcome_screen.dart';
 import 'package:petpal/screens/home/views/home_screen.dart';
 
@@ -23,7 +24,11 @@ class MyAppView extends StatelessWidget {
       home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
         builder: ((context, state) {
           if (state.status == AuthenticationStatus.authenticated) {
-            return const HomeScreen();
+            return BlocProvider(
+              create: (context) =>
+                  SignInBloc(context.read<AuthenticationBloc>().userRepository),
+              child: const HomeScreen(),
+            );
           } else {
             return const WelcomeScreen();
           }
--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,10 +1,52 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:petpal/screens/auth/blocs/sign_in_bloc/sign_in_bloc.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        title: Row(
+          children: [
+            Image.asset(
+              "assets/mobile.png",
+              scale: 14,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            const Text(
+              "Supplies",
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontSize: 30,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              CupertinoIcons.cart,
+            ),
+          ),
+          IconButton(
+            onPressed: () {
+              context.read<SignInBloc>().add(SignOutRequired());
+            },
+            icon: const Icon(
+              CupertinoIcons.arrow_right_to_line,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
