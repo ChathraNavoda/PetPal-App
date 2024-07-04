a9ec877a1e4c25c2f339c447edafc9db62e1922b
Chathra Navoda Ranasinghe <chathra96@gmail.com>
2024-07-04 14:08:26 +0530

    Create Sign in and Sign up blocs, views and text field component.

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:petpal/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:petpal/screens/auth/views/welcome_screen.dart';
+import 'package:petpal/screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: "Supplies Delivery",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          surface: Colors.grey.shade100,
+          onSurface: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: ((context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return const HomeScreen();
+          } else {
+            return const WelcomeScreen();
+          }
+        }),
+      ),
     );
   }
 }
